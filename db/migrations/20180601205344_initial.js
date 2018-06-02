9b80c521949afdc14bba94338f4c0cb9ce3ee36f
Patrick Rivera <pxr13@my.fsu.edu>
2018-06-01 21:39:29 -0400

    init migrations and seeds

---
+++ db/migrations/20180601205344_initial.js
@@ -0,0 +1,47 @@
+exports.up = (knex, Promise) =>
+  Promise.all([
+    knex.schema.createTable('project', (table) => {
+      table.increments('id').primary();
+      table.string('name', 128);
+      table.text('description');
+      table.boolean('is_complete').defaultTo(false);
+    }),
+
+    knex.schema.createTable('action', (table) => {
+      table.increments('id').primary();
+      table.integer('project_id').unsigned();
+      table.foreign('project_id').references('project.id');
+      table.text('description');
+      table.text('notes');
+      table.boolean('is_complete').defaultTo(false);
+    }),
+
+    knex.schema.createTable('context', (table) => {
+      table.increments('id').primary();
+      table.string('context');
+    }),
+
+    knex.schema.createTable('action_context', (table) => {
+      table.increments('id').primary();
+      table.integer('context_id').unsigned();
+      table.foreign('context_id').references('context.id');
+      table.integer('action_id').unsigned();
+      table.foreign('action_id').references('action.id');
+    }),
+
+    knex.schema.createTable('project_context', (table) => {
+      table.increments('id').primary();
+      table.integer('context_id').unsigned();
+      table.foreign('context_id').references('context.id');
+      table.integer('project_id').unsigned();
+      table.foreign('project_id').references('project.id');
+    }),
+  ]);
+
+exports.down = (knex, Promise) =>
+  Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('post_tags'),
+    knex.schema.dropTable('tags'),
+    knex.schema.dropTable('posts'),
+  ]);
